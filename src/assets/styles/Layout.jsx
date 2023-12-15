9e89a11c50328846eef82f5dc4ee7168ccf5e8ce
Moonchichiii <>
2023-12-15 10:51:55 +0000

    folder structure

---
+++ src/assets/styles/Layout.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Layout = ({ children }) => (
+  <>
+   <div className='navbars-container'>
+    
+     
+    </div>
+
+    <main>{children}</main> 
+    
+    
+    
+  </>
+);
+
+/*
+header or navbar 
+
+Main content display children input content coming in! 
+
+footer content 
+
+*/
+
+export default Layout;
