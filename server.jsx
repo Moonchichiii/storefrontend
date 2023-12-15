9d0e0358205e9786af7e3fe79b52e0328eb3ae01
Moonchichiii <>
2023-12-15 09:15:44 +0000

    secure start

---
+++ server.jsx
@@ -0,0 +1,23 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import compression from 'compression'; 
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+
+app.use(compression());
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
